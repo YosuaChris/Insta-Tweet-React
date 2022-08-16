b59cbdc81d8c845f6c69b47d4a6dac6d7518f35c
yosuavega <yosuavega3gp@gmail.com>
2022-08-16 14:37:20 +0700

    Trend card section

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,6 +4,7 @@ import Home from '../../img/home.png'
 import Noti from '../../img/noti.png'
 import Comment from '../../img/comment.png'
 import { UilSetting } from '@iconscout/react-unicons'
+import TrendCard from '../TrendCard/TrendCard'
 
 const RightSide = () => {
     return (
@@ -14,6 +15,8 @@ const RightSide = () => {
                 <img src={Noti} alt="" />
                 <img src={Comment} alt="" />
             </div>
+
+            <TrendCard />
         </div>
     )
 }
---
+++ src/components/TrendCard/TrendCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TrendCard.css'
+
+const TrendCard = () => {
+    return (
+        <div>TrendCard</div>
+    )
+}
+
+export default TrendCard
